f5a81b400660214eb6cdd2e276fd8ffe7fb39fae
ajlima12 <ajlimaa12@gmail.com>
2025-11-27 15:37:02 -0300

    first commit

---
+++ src/test/java/com/anajulia/modulo41/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anajulia.modulo41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
